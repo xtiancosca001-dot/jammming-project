165573fd8fd5ab781d780641f969eb692535ba60
Christian Eric Cosca <ccosca@iripple.com>
2025-05-23 15:43:26 +0800

    add components (static) + implement header first

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './header.module.css';
+function Header() {
+    return (
+        <header className={styles.header}>
+            <h1>Ja<span>mmm</span>ing</h1>
+        </header>
+    );
+}
+
+export default Header;
